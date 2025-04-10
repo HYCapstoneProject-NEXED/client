acea845afd1c94ccca790e434b046177780133ac
Kimbogyung <novo4756@gmail.com>
2025-04-10 17:52:32 +0900

    edit layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,13 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <div className="header">
-      <div className="user-role">Customer</div>
-      <div className="user-info">
-        <FaBell className="icon" />
-        <img src="/default-profile.jpg" alt="profile" className="profile-img" />
+    <div className="top-header">
+      <div className="header-right">
+        <div className="user-role">Customer</div>
+        <div className="user-info">
+          <FaBell className="icon" />
+          <img src="/default-profile.jpg" alt="profile" className="profile-img" />
+        </div>
       </div>
     </div>
   );
