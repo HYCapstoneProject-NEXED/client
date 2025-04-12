51efbaf8fe732cee9f9a4f65a07a915e24530484
Kimbogyung <novo4756@gmail.com>
2025-04-12 22:39:24 +0900

    ProfileModal 기능 추가

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './App.css'; // 스타일 필요시
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
